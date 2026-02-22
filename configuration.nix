35a89c995001ab0ea17e5068b0bb1c6e149d6b89
Shrey <swis000178@gmail.com>
2026-02-22 10:11:24 +0530

    Add files via upload

--- configuration.nix
+++ configuration.nix
@@ -1,190 +1,146 @@
-# Edit this configuration file to define what should be installed on
-# your system. Help is available in the configuration.nix(5) man page, on
-# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
-
 { config, lib, pkgs, ... }:
 
 {
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-    ];
+  imports = [ ./hardware-configuration.nix ];
 
-  # Use the systemd-boot EFI boot loader.
+  # Boot
   boot.loader.systemd-boot.enable = false;
-  boot.loader.grub.enable =  true;
+  boot.loader.grub.enable = true;
   boot.loader.grub.device = "nodev";
   boot.loader.grub.useOSProber = true;
   boot.loader.grub.efiSupport = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.efi.efiSysMountPoint = "/boot";
 
-  # Use latest kernel.
-  boot.kernelPackages = pkgs.linuxPackages_latest;
+  boot.kernelPackages = pkgs.linuxPackages_zen;   # same as your Arch
 
-    #FOR THE GPU AND IGPU SETTINGS
-
-    hardware.graphics = {
+  # ====================== GRAPHICS - NVIDIA + AMD HYBRID (your exact hardware) ======================
+  hardware.graphics = {
     enable = true;
     enable32Bit = true;
   };
 
-    environment.variables = {
-    AMD_VULKAN_ICD = "RADV";
-  };
-
-
-    hardware.nvidia = {
+  hardware.nvidia = {
     modesetting.enable = true;
     powerManagement.enable = true;
+    open = true;                    # matches your nvidia-open-dkms
     package = config.boot.kernelPackages.nvidiaPackages.stable;
+    nvidiaSettings = true;
+    prime = {
+      offload.enable = true;        # best battery + performance (Hybrid style)
+      offload.enableOffloadCmd = true;
+      amdgpuBusId = "PCI:106:0:0";  # your AMD iGPU (6a hex = 106 decimal)
+      nvidiaBusId   = "PCI:1:0:0";  # your RTX 5050
+    };
   };
 
+  environment.variables = {
+    AMD_VULKAN_ICD = "RADV";
+    NIXOS_OZONE_WL = "1";
+    __GLX_VENDOR_LIBRARY_NAME = "nvidia";   # from your hyprland.conf
+    GBM_BACKEND = "nvidia-drm";
+    LIBVA_DRIVER_NAME = "nvidia";
+    __GL_VRR_ALLOWED = "1";
+    __GL_GSYNC_ALLOWED = "1";
+  };
+
+  # NVIDIA offload wrapper (use for games: nvidia-offload steam / nvidia-offload lutris)
 
 
+  # ====================== HYPR LAND ======================
+  programs.hyprland = {
+    enable = true;
+    xwayland.enable = true;
+  };
 
+  services.displayManager.sddm = {
+    enable = true;
+    wayland.enable = true;
+  };
 
+  # ====================== ASUS ROG TOOLS (exact match to Arch) ======================
+  services.asusd = {
+    enable = true;
+    enableUserService = true;
+  };
 
-  networking.hostName = "SoS"; # Define your hostname.
-  # Pick only one of the below networking options.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-  networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
-  
-  nix.settings.experimental-features = ["nix-command" "flakes"];
+  services.supergfxd = {
+    enable = true;
+    # mode is controlled by /etc/supergfxd.conf (keeps your Hybrid setting)
+  };
 
-  # Set your time zone.
+  programs.rog-control-center.enable = true;
+
+  # ====================== AUDIO ======================
+  services.pipewire = {
+    enable = true;
+    pulse.enable = true;
+    jack.enable = true;
+    alsa.enable = true;
+  };
+  security.rtkit.enable = true;
+
+  # ====================== SYSTEM PACKAGES (all your important ones) ======================
+   
+   # for better wayland portals (screen sharing)
+   xdg.portal = {
+      enable = true;
+      extraPortals = [ pkgs.xdg-desktop-portal-hyprland ];
+   };
+   
+   #For bluetooth
+   services.blueman.enable = true;
+
+   #polkit (for password prompts in apps)
+   security.polkit.enable = true;
+
+  environment.systemPackages = with pkgs; [
+      
+      
+
+      (writeShellScriptBin "nvidia-offload" ''
+      export __NV_PRIME_RENDER_OFFLOAD=1
+      export __NV_PRIME_RENDER_OFFLOAD_PROVIDER=NVIDIA-G0
+      export __GLX_VENDOR_LIBRARY_NAME=nvidia
+      exec "$@"
+    '')
+
+    # Hyprland core (matches your hyprland.conf)
+    hyprland hyprcursor hyprpaper swww waybar swaynotificationcenter rofi wl-clipboard grim slurp
+    dunst cliphist wlogout networkmanagerapplet brightnessctl pamixer playerctl
+    foot wezterm kitty alacritty   # you had st, but these are better on NixOS
+    nautilus xfce.thunar easyeffects polkit_gnome blueman
+                        
+    # NVIDIA + gaming
+    mangohud gamemode steam lutris wine wineWowPackages.full protonup-ng obs-studio mpv
+
+    # Your CLI/dev tools
+    bat eza fd fzf ripgrep yazi fastfetch btop neovim git lazygit curl wget aria2 yt-dlp qbittorrent zathura
+
+    # Fonts + misc
+    noto-fonts noto-fonts-color-emoji nerd-fonts.fira-code nerd-fonts.jetbrains-mono nerd-fonts.hack
+    firefox ungoogled-chromium vscode obsidian
+
+    # ASUS & utils
+    asusctl supergfxctl
+  ];
+
+  # ====================== USER & SHELL ======================
+  users.users.moon = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" "networkmanager" "video" "audio" "libvirtd" "input" ];
+    shell = pkgs.zsh;
+  };
+
+  programs.zsh.enable = true;
+
+  # ====================== MISC ======================
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  nixpkgs.config.allowUnfree = true;
+
+  networking.hostName = "SoS";
+  networking.networkmanager.enable = true;
   time.timeZone = "Asia/Kolkata";
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Select internationalisation properties.
-  # i18n.defaultLocale = "en_US.UTF-8";
-  # console = {
-  #   font = "Lat2-Terminus16";
-  #   keyMap = "us";
-  #   useXkbConfig = true; # use xkb.options in tty.
-  # };
-
-  # Enable the X11 windowing system.
-  # services.xserver.enable = true;
-   # services.xserver.windowManager.dwm = {
-   #	enable = true;
-#	package = pkgs.dwm.overrideAttrs{
-#		src = ./config/dwm;
-#	};
- #   };
-
-
-    services.xserver = {
-       enable =true;
-       windowManager.qtile.enable = true;
-    };
-
-  
-
-  # Configure keymap in X11
-  # services.xserver.xkb.layout = "us";
-  # services.xserver.xkb.options = "eurosign:e,caps:escape";
-
-  # Enable CUPS to print documents.
-  # services.printing.enable = true;
-
-  # Enable sound.
-  # services.pulseaudio.enable = true;
-  # OR
-   services.pipewire = {
-     enable = true;
-     pulse.enable = true;
-   };
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.libinput.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-     users.users.moon = {
-     isNormalUser = true;
-     extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
-     packages = with pkgs; [
-       tree
-     ];
-   };
-  
-  
-  nixpkgs.config.allowUnfree = true;
-  programs.firefox.enable = true;
-
-  # List packages installed in system profile.
-  # You can use https://search.nixos.org/ to find more packages (and options).
-   environment.systemPackages = with pkgs; [
-     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-     wget
-     git
-     wezterm
-     neovim
-     btop
-     curl
-     bat
-     tealdeer
-     xclip
-     bat
-     os-prober
-     yazi
-     nodejs_24
-     lua5_1
-     lua
-     lazygit
-     xfce.thunar
-     mesa
-     vulkan-tools
-   ];
-
-
-   programs.zsh.enable = true;
-   users.defaultUserShell = pkgs.zsh;
-
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # Copy the NixOS configuration file and link it from the resulting system
-  # (/run/current-system/configuration.nix). This is useful in case you
-  # accidentally delete configuration.nix.
-  # system.copySystemConfiguration = true;
-
-  # This option defines the first version of NixOS you have installed on this particular machine,
-  # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
-  #
-  # Most users should NEVER change this value after the initial install, for any reason,
-  # even if you've upgraded your system to a new NixOS release.
-  #
-  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
-  # so changing it will NOT upgrade your system - see https://nixos.org/manual/nixos/stable/#sec-upgrading for how
-  # to actually do that.
-  #
-  # This value being lower than the current NixOS release does NOT mean your system is
-  # out of date, out of support, or vulnerable.
-  #
-  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
-  # and migrated your data accordingly.
-  #
-  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
-  system.stateVersion = "25.05"; # Did you read the comment?
-
+  system.stateVersion = "25.05";
 }
-
